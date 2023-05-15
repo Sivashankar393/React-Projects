ea19102f66af2a2ea23349a0479ae4f87a7443c5
Sivashankar393 <Sivashankar393@gmail.com>
2023-05-15 23:59:22 +0530

    ok

--- Travels Website/src/components/ContactForm.js
+++ Travels Website/src/components/ContactForm.js
@@ -3,7 +3,7 @@ import "./ContactFormStyles.css";
 function ContactForm() {
   return (
     <div className="from-container">
-      <h1>Your Code Here</h1>lllllll
+      <h1>Your Code Here</h1>
     </div>
   );
 }
--- Travels Website/src/components/Navbar.js
+++ Travels Website/src/components/Navbar.js
@@ -4,13 +4,31 @@ import { MenuItems } from "./MenuItems";
 import { Link } from "react-router-dom";
 
 class Navbar extends Component {
-  // Set state
-  // Make Handleclick Function
+
+  state={clicked:false};
+  handleClick=()=>{
+    this.setState=({clicked:!this.state.clicked})
+  }
 
   render() {
     return (
       <nav className="NavbarItems">
-        <h1>Your Code Here</h1>
+        <h1>Narasimma Travels</h1>
+
+        <div className="menu-icons" onClick={this.handleClick}>
+          <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
+
+        <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+          {MenuItems.map((item,index)=>{
+            return(
+              <li key={index}>
+              <Link className={item.cName} to={item.url}><i className={item.icon}></i>{item.title}</Link>
+            </li>
+            )
+          })}
+          <button>Sign Up</button>
+        </ul>
       </nav>
     );
   }
